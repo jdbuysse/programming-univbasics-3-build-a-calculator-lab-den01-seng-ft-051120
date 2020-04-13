c14401702e9b527864acec6b03c6dd64aa6d8b29
Jordan Buysse <jdbuysse@gmail.com>
2020-04-13 12:52:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,20 @@
 def addition(num1, num2)
-
+  res = num1 + num2
+  res
 end
 
 def subtraction(num1, num2)
-
+  res = num1 - num2
+  res
 end
 
 def division(num1, num2)
-
+  res = num1 % num2
+  res
 end
 
 def multiplication(num1, num2)
-
+  return num1 * num2
 end
 
 def modulo(num1, num2)
